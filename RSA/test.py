ddb7b4b3552bbc0c8be6793168c1f395ca54bc4f
Sudipta Basak <sudiptab2100@gmail.com>
2023-04-18 12:15:23 +0530

    1024-bit key RSA

--- RSA/test.py
+++ RSA/test.py
@@ -11,11 +11,11 @@ d = int(creds['d'], 16)
 with open("RSA/IITJammu.png", "rb") as f: 
     m = f.read()
     c = encrypt(m, e, n)
-    with open("RSA/cipher", "wb") as op: pickle.dump(c, op)
-print('Encrypted')
+    with open("RSA/cipher.dat", "wb") as op: pickle.dump(c, op)
+    print('Encrypted')
 
-with open("RSA/cipher", "rb") as f:
+with open("RSA/cipher.dat", "rb") as f:
     c = pickle.load(f)
     dm = decrypt(c, d, n)
     with open("RSA/decrypted_IITJammu.png", "wb") as op: op.write(bytearray(dm))
-print('Decrypted')
+    print('Decrypted')
